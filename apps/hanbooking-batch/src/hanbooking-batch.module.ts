a50894733af29008d53a756bd789ba4283d9c0ef
Nurislom ( N E O ) <nurislomkhurramov@gmail.com>
2026-02-23 14:20:33 +0900

    feat: configure env variables

--- apps/hanbooking-batch/src/hanbooking-batch.module.ts
+++ apps/hanbooking-batch/src/hanbooking-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HanbookingBatchController } from './hanbooking-batch.controller';
 import { HanbookingBatchService } from './hanbooking-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [HanbookingBatchController],
   providers: [HanbookingBatchService],
 })
