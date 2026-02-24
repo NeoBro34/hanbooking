86fe70d795f042ca12075edeae9e3543ba4f6a78
Nurislom ( N E O ) <nurislomkhurramov@gmail.com>
2026-02-24 15:24:06 +0900

    feat: integrate jwt & develop creation and verification jwt

---
+++ apps/hanbooking-api/src/components/auth/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
+import { Member } from '../../libs/dto/member/member';
+import { T } from '../../libs/types/common';
+import { shapeIntoMongoObjectId } from '../../libs/config';
+
+@Injectable()
+export class AuthService {
+    constructor(private jwtService: JwtService) {}
+
+    /** hashPassword **/
+    public async hashPassword(memberPassword: string): Promise<string> {
+        const salt = await bcrypt.genSalt();
+        return await bcrypt.hash(memberPassword, salt);
+    }
+
+    /** comparePasswords **/
+    public async comparePasswords(password: string, hashedPassword:  string): Promise<boolean> {
+        return await bcrypt.compare(password, hashedPassword);
+    }
+
+    /** createToken **/
+    public async createToken(member: Member): Promise<string> {
+        const payload: T = {};
+        Object.keys(member['_doc'] ? member['_doc'] : member).map((ele) => {
+            payload[`${ele}`] = member[`${ele}`];
+        });
+        delete payload.memberPassword;
+        return await this.jwtService.signAsync(payload);
+    }
+
+    /** verifyToken **/
+    public async verifyToken(token: string): Promise<Member> {
+        const member = await this.jwtService.verifyAsync(token);
+        member._id = shapeIntoMongoObjectId(member._id);
+        return member;
+    }
+}
